4627a7fce8ea8d4d625488aa5e6dea592fa60de5
joseph <596213836@qq.com>
2021-04-17 15:29:21 +0800

    v1.0.2

---
+++ futuretask/src/main/java/com/luck/controller/TestDate.java
@@ -0,0 +1,13 @@
+package com.luck.controller;
+
+/**
+ * @author : Joseph.Tan
+ * @date : 2021/3/9 8:34
+ */
+public class TestDate {
+
+    public static void main(String[] args) {
+
+        System.out.println("天下第一张三丰");
+    }
+}
